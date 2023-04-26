4ea5cef897d33351974f66be89b50fc8745671cc
Rodrigo Francia <rodrigofrancia98@gmail.com>
2023-04-26 20:07:05 -0300

    header formatted

--- src/assets/header.jsx
+++ src/assets/header.jsx
@@ -1,13 +1,11 @@
 
-function Header(){
+function Header() {
 
-return (
-    
-    <h1 className="font-black text-4xl  mt-10 text-center">Seguimiento Pacientes
-    <span className="text-indigo-600"> Veterinaria</span>
-    </h1>
-    
-)
+    return (
+        <h1 className="font-black text-4xl  mt-10 text-center">Seguimiento Pacientes
+            <span className="text-indigo-600"> Veterinaria</span>
+        </h1>
+    )
 
 }
 
